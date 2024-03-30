bfa2158e65ea7e7512413094e4b4edec91f7e212
alekhya-korsa2612 <150769034+alekhya-korsa2612@users.noreply.github.com>
2024-03-30 15:01:26 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,25 +2,27 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const authRouter = require('./routes/authRouter');
+
 const app = express();
 const PORT = process.env.PORT || 8000;
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use('/api', authRouter);
-
-mongoose.connect('mongodb://localhost:27017/authentication', {
+const uri = 'mongodb+srv://korsaalekhya516:<6bA91pJwoK0rGY6w>@cluster0.wq9diur.mongodb.net/';
+mongoose.connect(uri, {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('Connected to MongoDB Atlas with Mongoose');
+}).catch((error) => {
+  console.error('Error connecting to MongoDB Atlas with Mongoose:', error);
 });
-const db = mongoose.connection;
-db.on('error', (error) => console.error('MongoDB connection error:', error));
-db.once('open', () => console.log('Connected to Database'));
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
+
 
 
 
